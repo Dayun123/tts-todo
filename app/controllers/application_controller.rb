8a46768003fc4c83d2083bc575d596ef008062d9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-07-05 10:50:18 -0500

    Add authenticate method to application controller.
    Add 'new' action to SessionsController.
    Make users sign in to work with tasks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
+    def authenticate!
+      redirect_to :signin unless current_user
+    end
+
 end
