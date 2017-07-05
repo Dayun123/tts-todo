ec48e189171017df0c35d8a1451f40e070c14a93
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-07-05 09:15:11 -0500

    Setup user model.
    Add method that allows model to create a user from FB login.

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+
+  # Get the stuff from Facebook and make a user
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      user.name = auth['info']['name']
+    end
+  end
+
+  # Grab the Facebook profile picture
+  def small_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=small"
+  end
+
+  def normal_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=normal"
+  end
+
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  provider   :string
+#  uid        :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
