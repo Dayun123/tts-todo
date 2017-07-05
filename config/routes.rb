675a4d3d10d9c28ceea2a0ee510456f0442a52d4
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-07-05 09:31:41 -0500

    Setup sessions controller.
    Create current_user helper.
    Add sessions routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signout' => 'sessions#destroy', as: :signout
+
   resources :user_tasks
 
   root 'user_tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
