55945358b9f349b07430c2fb28d807c1c5bc5534
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-07-05 08:03:37 -0500

    Add annotate and JQuery.

--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: user_tasks
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  due         :date
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class UserTask < ApplicationRecord
 end
--- test/models/user_task_test.rb
+++ test/models/user_task_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: user_tasks
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  due         :date
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTaskTest < ActiveSupport::TestCase
